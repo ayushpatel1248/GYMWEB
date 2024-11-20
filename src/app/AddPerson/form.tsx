1be0ae781b34a3211d15bccc3cf110f081257b57
ayush <ayushpatel1248@gmail.com>
2024-11-20 15:42:31 +0530

    drop down corrected membership plan

--- src/app/AddPerson/form.tsx
+++ src/app/AddPerson/form.tsx
@@ -231,7 +231,7 @@ export default function Form() {
               <Menu as="div" className="relative inline-block text-left">
                 <div>
                   <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                    Membership Plan
+                    {membershipPlan} Membership Plan
                     <ChevronDownIcon
                       aria-hidden="true"
                       className="-mr-1 h-5 w-5 text-gray-400"
