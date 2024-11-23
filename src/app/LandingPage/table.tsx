86304bf7a5973b125c20c25d8f6285ae68aa7b73
ayush <ayushpatel1248@gmail.com>
2024-11-23 14:08:31 +0530

    LINK fix

--- src/app/LandingPage/table.tsx
+++ src/app/LandingPage/table.tsx
@@ -234,18 +234,16 @@ const Table = () => {
                       <div className="w-10 h-10 bg-gray-200 rounded-full"></div>
                     )}
                   </td>
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  <Link
+                    href={`/aboutPerson?${new URLSearchParams(row).toString()}`}
                   >
-                    <Link
-                      href={`/aboutPerson?${new URLSearchParams(
-                        row
-                      ).toString()}`}
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                     >
                       {row.fullName}
-                    </Link>
-                  </th>
+                    </th>
+                  </Link>
                   <td className="px-6 py-4">{row.doj}</td>
                   {/* <td className="px-6 py-4">{row.membershipEndDate}</td> */}
                   <td className="px-6 py-4">{row.totalfees}</td>
