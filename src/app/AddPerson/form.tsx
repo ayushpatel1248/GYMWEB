8dccf80738f69fb11083c785b1b5349c63259033
harshit nagar <nharshit7024@gmail.com>
2024-11-30 18:09:43 +0530

    add photo ui updated

--- src/app/AddPerson/form.tsx
+++ src/app/AddPerson/form.tsx
@@ -5,7 +5,7 @@ import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { createClient } from "../../utils/supabase/client";
 import { ChangeEvent, useState, FormEvent, useEffect } from "react";
-
+import "./addperson.css"
 // Function to compress image
 const compressImage = async (file: File): Promise<File> => {
   return new Promise((resolve, reject) => {
@@ -176,18 +176,53 @@ export default function Form() {
                 >
                   Add Photo
                 </label>
+
                 <div className="mt-2 flex items-center gap-x-3">
                   <UserCircleIcon
                     aria-hidden="true"
                     className="h-12 w-12 text-gray-300"
                   />
-                  <input
+                  {/* add photo statrt */}
+                  <div>
+                    <span>
+                      <div className="file-input">
+                        <input
+                          type="file"
+                          name="file-input"
+                          id="file-input"
+                          className="file-input__input-profile"
+                          accept='image/*'
+                          onChange={handleFileChange}
+                        />
+                        <label className="file-input__label-profile" htmlFor="file-input">
+                          <svg
+                            aria-hidden="true"
+                            focusable="false"
+                            data-prefix="fas"
+                            data-icon="upload"
+                            className="svg-inline--fa fa-upload fa-w-16"
+                            role="img"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
+                            ></path>
+                          </svg>
+                          <span>{imageFile?.name ?? "Update Image Photo"}</span></label>
+                      </div>
+                    </span>
+                  </div>
+
+                  {/* add photo endds */}
+                  {/* <input
                     type="file"
                     accept="image/*"
                     // capture
                     onChange={handleFileChange}
                     className="block w-full text-sm text-gray-900"
-                  />
+                  /> */}
                 </div>
               </div>
             </div>
@@ -382,7 +417,7 @@ export default function Form() {
             )}
           </button>
         </div>
-      </form>
-    </div>
+      </form >
+    </div >
   );
 }
