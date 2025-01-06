fe3db724c11576daa3237fa3371bd935d5fca277
harshit nagar <nharshit7024@gmail.com>
2025-01-06 23:55:17 +0530

    dark theam in add person correction

--- src/app/AddPerson/form.tsx
+++ src/app/AddPerson/form.tsx
@@ -362,7 +362,7 @@ export default function Form() {
                     type="text"
                     value={fullName}
                     onChange={(e) => setFullName(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-zinc-900 dark:text-white"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 dark:bg-zinc-900 dark:text-white"
                   />
                 </div>
               </div>
@@ -381,7 +381,7 @@ export default function Form() {
                     type="number"
                     value={mobileNumber}
                     onChange={(e) => setMobileNumber(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-zinc-900 dark:text-white"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 dark:bg-zinc-900 dark:text-white"
                   />
                 </div>
               </div>
@@ -399,7 +399,7 @@ export default function Form() {
                     type="number"
                     value={weight}
                     onChange={(e) => setWeight(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-zinc-900 dark:text-white"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 dark:bg-zinc-900 dark:text-white"
                   />
                 </div>
               </div>
@@ -446,7 +446,7 @@ export default function Form() {
                     type="date"
                     value={dateOfJoin}
                     onChange={(e) => setDateOfJoin(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-zinc-900 dark:text-white"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 dark:bg-zinc-900 dark:text-white"
                   />
                 </div>
               </div>
@@ -464,7 +464,7 @@ export default function Form() {
                     type="number"
                     value={totalFees}
                     onChange={(e) => setTotalFees(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-zinc-900 dark:text-white"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 dark:bg-zinc-900 dark:text-white"
                   />
                 </div>
               </div>
